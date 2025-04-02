2a576c21cedc30fbb39eb247fd83dbe177f74cfe
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-03 01:56:17 +0530

    Decorated the CategoryController class using @RestController annotation to take network request

--- src/main/java/edu/popcorn/eCom/sample3/controller/CategoryController.java
+++ src/main/java/edu/popcorn/eCom/sample3/controller/CategoryController.java
@@ -2,10 +2,12 @@ package edu.popcorn.eCom.sample3.controller;
 
 import edu.popcorn.eCom.sample3.model.Category;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class CategoryController {
     List<Category> categories = new ArrayList<>();
 
