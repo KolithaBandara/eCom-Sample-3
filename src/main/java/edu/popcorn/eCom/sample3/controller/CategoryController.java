0c298f8fc26d9baa9efa5e27ebab73f60ae04886
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-03 01:49:33 +0530

    Defined a list array to store the categories

--- src/main/java/edu/popcorn/eCom/sample3/controller/CategoryController.java
+++ src/main/java/edu/popcorn/eCom/sample3/controller/CategoryController.java
@@ -1,4 +1,11 @@
 package edu.popcorn.eCom.sample3.controller;
 
+import edu.popcorn.eCom.sample3.model.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryController {
+    List<Category> categories = new ArrayList<>();
+    
 }
