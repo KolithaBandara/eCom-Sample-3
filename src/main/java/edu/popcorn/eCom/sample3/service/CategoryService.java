354a726e7b367732985382ae8bbe036918ae456b
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-03 02:18:39 +0530

    Created the CategoryService interface

--- src/main/java/edu/popcorn/eCom/sample3/service/CategoryService.java
+++ src/main/java/edu/popcorn/eCom/sample3/service/CategoryService.java
@@ -1,4 +1,10 @@
 package edu.popcorn.eCom.sample3.service;
 
+import edu.popcorn.eCom.sample3.model.Category;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public interface CategoryService {
+    String addCategory(@RequestBody Category category);
+    String removeCategory(@PathVariable Long categoryId);
 }
