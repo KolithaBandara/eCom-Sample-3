2bc24c8033e7e66de00895f7ffe008a8ad7f6346
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-03 02:02:16 +0530

    Modified the addCategory method

--- src/main/java/edu/popcorn/eCom/sample3/controller/CategoryController.java
+++ src/main/java/edu/popcorn/eCom/sample3/controller/CategoryController.java
@@ -2,6 +2,8 @@ package edu.popcorn.eCom.sample3.controller;
 
 import edu.popcorn.eCom.sample3.model.Category;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,8 +17,10 @@ public class CategoryController {
     public List<Category> getAllCategories(){
         return categories;
     }
-    
-    public String addCategory(){
+
+    @PostMapping("api/admin/category")
+    public String addCategory(@RequestBody Category category){
+        categories.add(category);
         return "Category added successfully!";
     }
 }
