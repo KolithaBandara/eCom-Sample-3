e339586ba58bc1fa9ee812353ef1be74969760d5
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-03 01:52:37 +0530

    Decorate the CategoryController class using @GetMapping annotation to handle the network request from the front-end

--- src/main/java/edu/popcorn/eCom/sample3/controller/CategoryController.java
+++ src/main/java/edu/popcorn/eCom/sample3/controller/CategoryController.java
@@ -1,13 +1,15 @@
 package edu.popcorn.eCom.sample3.controller;
 
 import edu.popcorn.eCom.sample3.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CategoryController {
     List<Category> categories = new ArrayList<>();
-    
+
+    @GetMapping()
     public List<Category> getAllCategories(){
         return categories;
     }
