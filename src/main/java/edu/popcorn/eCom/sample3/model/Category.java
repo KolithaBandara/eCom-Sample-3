5302bbcfce9f6c235f5495bd5beecbb4d18f802c
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-03 01:39:34 +0530

    Created the all args constructor for the Category class

--- src/main/java/edu/popcorn/eCom/sample3/model/Category.java
+++ src/main/java/edu/popcorn/eCom/sample3/model/Category.java
@@ -3,4 +3,9 @@ package edu.popcorn.eCom.sample3.model;
 public class Category {
     private Long id;
     private String name;
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
