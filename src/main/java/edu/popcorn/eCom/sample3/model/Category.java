b78e67c97d15787742294aa8e4366aea52e729a7
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-03 01:46:38 +0530

    Decorated the Category class using @AllArgsConstructor annotation to set All Argument Constructor to runtime

--- src/main/java/edu/popcorn/eCom/sample3/model/Category.java
+++ src/main/java/edu/popcorn/eCom/sample3/model/Category.java
@@ -1,14 +1,11 @@
 package edu.popcorn.eCom.sample3.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Category {
     private Long id;
     private String name;
-
-    public Category(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 }
