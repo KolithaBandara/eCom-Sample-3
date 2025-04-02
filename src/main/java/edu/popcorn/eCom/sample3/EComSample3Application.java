376fbb0146aaf4dbd8395069b537d010adc5682e
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-03 01:34:22 +0530

    Inizialized the ecom project

---
+++ src/main/java/edu/popcorn/eCom/sample3/EComSample3Application.java
@@ -0,0 +1,13 @@
+package edu.popcorn.eCom.sample3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComSample3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComSample3Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/popcorn/eCom/sample3/EComSample3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.popcorn.eCom.sample3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComSample3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
