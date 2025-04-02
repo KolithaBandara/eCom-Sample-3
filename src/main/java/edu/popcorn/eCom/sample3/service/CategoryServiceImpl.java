269715c08d05586b50e94d4a0cea9a3760560f48
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-03 02:20:37 +0530

    Implements the Category Service

--- src/main/java/edu/popcorn/eCom/sample3/service/CategoryServiceImpl.java
+++ src/main/java/edu/popcorn/eCom/sample3/service/CategoryServiceImpl.java
@@ -1,4 +1,15 @@
 package edu.popcorn.eCom.sample3.service;
 
-public class CategoryServiceImpl {
+import edu.popcorn.eCom.sample3.model.Category;
+
+public class CategoryServiceImpl implements CategoryService{
+    @Override
+    public String addCategory(Category category) {
+        return "";
+    }
+
+    @Override
+    public String removeCategory(Long categoryId) {
+        return "";
+    }
 }
