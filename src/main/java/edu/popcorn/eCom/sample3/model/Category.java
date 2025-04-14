ea833fb6c0791018a18bd94f7dc836dd44b6243b
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-14 22:57:39 +0530

    Decorated using @NoArgsConstructor annotation

--- src/main/java/edu/popcorn/eCom/sample3/model/Category.java
+++ src/main/java/edu/popcorn/eCom/sample3/model/Category.java
@@ -2,9 +2,11 @@ package edu.popcorn.eCom.sample3.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Category {
     private Long id;
     private String name;
