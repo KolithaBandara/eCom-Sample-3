fd0c41fd593f4bfea8a8d6ca70ef941fa26b936d
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-16 23:35:25 +0530

    Update CategoryController.java

--- src/main/java/edu/popcorn/eCom/sample3/controller/CategoryController.java
+++ src/main/java/edu/popcorn/eCom/sample3/controller/CategoryController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class CategoryController {
     CategoryService categoryService = new CategoryServiceImpl();
     private Long id = 0L;
+    
 
     @GetMapping("api/public/categories")
     public List<Category> getAllCategories(){
